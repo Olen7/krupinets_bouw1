8790336944ef7b8eff3bb4ecc0b3b59924a50cd3
Olen7 <olenmykola@gmail.com>
2023-10-28 18:34:14 +0200

    work_7

--- js/review.js
+++ js/review.js
@@ -1,14 +1,14 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
+      openModalBtn: document.querySelector("[data-review-open]"),
+      closeModalBtn: document.querySelector("[data-review-close]"),
+      modal: document.querySelector("[data-review]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+      refs.modal.classList.toggle("is-hided");
     }
   })();
