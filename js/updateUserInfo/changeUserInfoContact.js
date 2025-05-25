62ea9d8f7f1b82a92b2637059ebc7341be8716bf
Mykola Olen <olenmykola@gmail.com>
2025-05-25 18:02:31 +0200

    Integrated centralized contact access

---
+++ js/updateUserInfo/changeUserInfoContact.js
@@ -0,0 +1,32 @@
+export function updatePhoneContact() {
+  const updatePhoneEl = document.querySelector("#update-phone-contact");
+  console.log(updatePhoneEl);
+  updatePhoneEl.insertAdjacentHTML(
+    "afterend",
+    `<a href="${contact.phone}" class="address_link_contact">${contact.phone}</a>`
+  );
+}
+function updateEmailContact() {
+  const updateEmailEl = document.querySelector("#update-email-contact");
+  updateEmailEl.insertAdjacentHTML(
+    "beforeend",
+    `<a href="mailto:${contact.email}" class="address_link_contact">${contact.email}</a>`
+  );
+  localStorage.setItem("email", contact.email);
+}
+
+function updateAddressContact() {
+  const updateAddressEl = document.querySelector("#update-address-contact");
+  updateAddressEl.insertAdjacentHTML(
+    "beforeend",
+    `<a href="https://maps.app.goo.gl/WqgaP1G8ZHufNCVWA"
+        class="address_link_contact"
+        target="_blank"
+        rel="noopener noreferrer nofollow">${contact.address}
+    </a>`
+  );
+  localStorage.setItem("address", contact.address);
+}
+updatePhoneContact();
+updateEmailContact();
+updateAddressContact();
